82885b1e3c4c80bf42472eb22c5f7a39f6fb3524
SergeyKosyanyuk <ser.ser.05021997@gmail.com>
2019-03-10 15:40:34 +0300

    add sever esp collection

--- server/routes/api/posts.js
+++ server/routes/api/posts.js
@@ -4,15 +4,27 @@ const mongodb = require('mongodb');
 const router = express.Router();
 let client;
 let connect = false;
+
+
+//Esp32S Server
+router.post('/esp/', async (req, res) => {
+    const posts = await loadPostsCollection('esp');
+    await posts.insertOne({
+        text: req.body.text,
+        type: req.body.type
+    });
+    res.status(201).send();
+});
+
 // Get Posts
 router.get('/', async (req, res) => {
-    const posts = await loadPostsCollection();
+    const posts = await loadPostsCollection('posts');
     res.send(await posts.find({}).toArray());
 });
 
 // Add post
 router.post('/', async (req, res) => {
-    const posts = await loadPostsCollection();
+    const posts = await loadPostsCollection('posts');
     await posts.insertOne({
         text: req.body.text,
         createdAt: new Date()
@@ -22,12 +34,12 @@ router.post('/', async (req, res) => {
 
 // Delete Post
 router.delete('/:id', async (req, res) => {
-    const posts = await loadPostsCollection();
+    const posts = await loadPostsCollection('posts');
     await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
     res.status(200).send();
 });
 
-async function loadPostsCollection(){
+async function loadPostsCollection(collect){
     if(!connect) {
         client = await mongodb.MongoClient.connect('mongodb+srv://sergey_admin_22:5VAs8wkFx5fXTrr@cluster0-whxsz.gcp.mongodb.net/vue_express?retryWrites=true', {
             useNewUrlParser: true
@@ -35,7 +47,7 @@ async function loadPostsCollection(){
         connect = true;
     }
 
-    return client.db('vue_express').collection('posts');
+    return client.db('vue_express').collection(collect);
 }
 
 
